b63822fb400abbd349da8549e25ee0ba8a5f8c4b
Arth-t-carvalho <arthur.t.carvalho@aluno.senai.br>
2026-03-11 17:25:57 -0300

    feat(management): add controllers for infractions and management modules

---
+++ src/Presentation/Controller/InfractionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace epiGuard\Presentation\Controller;
+
+class InfractionController
+{
+    public function index()
+    {
+        require_once __DIR__ . '/../View/infractions/index.php';
+    }
+}
